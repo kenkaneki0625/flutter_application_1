3767da8784a5d7b2ea4873d1cf40839b5784ea31
Shaon Baidya <kenkaneki0625@gmail.com>
2022-01-17 17:36:49 +0530

    Hero widget (#4)

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -3,6 +3,8 @@ import 'package:provider/provider.dart';
 import '../widget/navigation_drawer_widget.dart';
 import '../provider/theme_provider.dart';
 import '../widget/change_theme_button_widget.dart';
+import '../page/people_page.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -19,15 +21,28 @@ class HomePage extends StatelessWidget {
           ChangeThemeButtonWidget(),
         ],
       ),
-      body: Center(
-        child: Text(
-          'Hello $text!',
-          style: TextStyle(
-            fontSize: 32,
-            fontWeight: FontWeight.bold,
-          ),
+      body: GestureDetector(
+        onTap: ()=>Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => PeoplePage(),
+        )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+                'Hello $text!',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+                ),
+            Hero(
+              tag: "kimetsunoyaiba",
+              child :Image.network("https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/05/Tanjiro-and-Zenitsu-featured-from-demon-slayer-Cropped.jpg?q=50&fit=crop&w=767&h=450&dpr=1.5",
+            width: 200,))
+          ],
         ),
-      ),
-    );
+        )
+            );
   }
 }
