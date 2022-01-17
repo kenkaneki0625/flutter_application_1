ec17b081735af1820f7e92c28b24cdf0197ae419
Shaon Baidya <kenkaneki0625@gmail.com>
2022-01-17 17:35:52 +0530

    Hero widget

--- lib/page/people_page.dart
+++ lib/page/people_page.dart
@@ -9,5 +9,9 @@ class PeoplePage extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.green,
         ),
+        body: Hero(
+              tag: "kimetsunoyaiba",
+              child :Image.network("https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/05/Tanjiro-and-Zenitsu-featured-from-demon-slayer-Cropped.jpg?q=50&fit=crop&w=767&h=450&dpr=1.5",
+            width: 400,)),
       );
 }
